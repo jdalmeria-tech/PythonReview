8590a6237d1cf7d323817c5711320cc93bbeee7a
jdalmeria-tech <alm24008@byui.edu>
2025-12-18 10:39:23 -1000

    basic input and output

---
+++ input_output.py
@@ -0,0 +1,11 @@
+"""
+This program will follow the iconic James Bond intro style
+"Bond, James Bond."
+It will print out the phrase in a stylized manner.
+"""
+# Get user input for first and last name
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+
+# Display the iconic phrase
+print(f"Your name is {last_name}, {first_name} {last_name}.")
