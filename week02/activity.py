7aa0dcd401e39e3648bfd3c0e2b612faba8e1cc4
jdalmeria-tech <alm24008@byui.edu>
2025-12-20 17:48:19 -1000

    basic muna

--- week02/activity.py
+++ week02/activity.py
@@ -1,13 +1,18 @@
-age = int(input("How old are you? "))
+"""
+Author: Joseph Almeria
+Purpose: Practice using mathematical expressions in Python.
+"""
+
+"""age = int(input("How old are you? "))
 age_next_year = age + 1
-print(f"On your next birthday, you will be {age_next_year} years old.")
+print(f"On your next birthday, you will be {age_next_year} years old.\n")
 
 egg_cartons = int(input("How many egg cartons do you have? "))
 eggs_per_carton = 12
 total_eggs = egg_cartons * eggs_per_carton
-print(f"You have a total of {total_eggs} eggs.")
+print(f"You have a total of {total_eggs} eggs.\n")"""
 
-cookies = float(input("How many cookies do you have? "))
+cookies = int(input("How many cookies do you have? "))
 cookies_per_person = int(input("How many people are there? "))
 cookies_shared = cookies / cookies_per_person
 print(f"Each person gets {cookies_shared:.2f} cookies.")
