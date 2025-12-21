3546d1d51fef1de9575415aa198711b59b2ae7b6
jdalmeria-tech <alm24008@byui.edu>
2025-12-20 17:48:54 -1000

    removed the comment

--- week02/activity.py
+++ week02/activity.py
@@ -3,14 +3,14 @@ Author: Joseph Almeria
 Purpose: Practice using mathematical expressions in Python.
 """
 
-"""age = int(input("How old are you? "))
+age = int(input("How old are you? "))
 age_next_year = age + 1
 print(f"On your next birthday, you will be {age_next_year} years old.\n")
 
 egg_cartons = int(input("How many egg cartons do you have? "))
 eggs_per_carton = 12
 total_eggs = egg_cartons * eggs_per_carton
-print(f"You have a total of {total_eggs} eggs.\n")"""
+print(f"You have a total of {total_eggs} eggs.\n")
 
 cookies = int(input("How many cookies do you have? "))
 cookies_per_person = int(input("How many people are there? "))
